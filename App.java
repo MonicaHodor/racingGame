4fdf93ad88e234a38a6812f84ac966f0a87a723d
MonicaHodor <monicahodor32@gmail.com>
2018-06-06 11:33:11 +0300

    added Arrays and List competitors.

--- App.java
+++ App.java
@@ -7,9 +7,27 @@ public class App {
         Game game = new Game();
         game.start();
 
+        // Arrays utility example
+
+        String text = "100.00 RON";
+        String[] result = text.split(" ");
+
+        for (String resultElement : result) {
+            System.out.println(resultElement);
 
 
+            String priceAsString = result[0];
+            System.out.println("Price as string before replace:" + priceAsString);
+
+            priceAsString = priceAsString.replace(",", ".");
+            System.out.println("Price as string after replace:" + priceAsString);
+
+            double price = Double.parseDouble(priceAsString);
+
+            System.out.println("Parcedprice:" + price);
+
+
+        }
 
     }
-
 }
--- Game.java
+++ Game.java
@@ -1,52 +1,69 @@
  package org.fasttrackit;
 
 
+ import java.util.ArrayList;
+ import java.util.List;
+
  public class Game {
 
- Car firstCompetitor;
- Car secondCompetitor;
+     Vehicle[] competitors = new Vehicle[10];
+     List<Vehicle> competitorsList = new ArrayList<>();
+     Vehicle firstCompetitor;
+     Vehicle secondCompetitor;
 
 
- public void start () {
-     Car car1 = new Car("Dacia");
-     car1.setColor("red");
-     car1.setDoorCount(3);
-     car1.setMileage(7.5);
-     car1.setFuelLevel(50);
-     Engine engine1 = new Engine();
-     engine1.setManufacturer("Renault");
+     public void start() {
+         Car car1 = new Car("Dacia");
+         car1.setColor("red");
+         car1.setDoorCount(3);
+         car1.setMileage(7.5);
+         car1.setFuelLevel(50);
+         Engine engine1 = new Engine();
+         engine1.setManufacturer("Renault");
 
-     Car car2 = new Car("Ford");
-     car2.setColor("black");
-     car2.setDoorCount(2);
-     car2.setMileage(8.2);
-     car2.setFuelLevel(4.7);
+         Car car2 = new Car("Ford");
+         car2.setColor("black");
+         car2.setDoorCount(2);
+         car2.setMileage(8.2);
+         car2.setFuelLevel(4.7);
 
-     Engine engine2 = new Engine();
-     engine2.setManufacturer("Ford");
+         Engine engine2 = new Engine();
+         engine2.setManufacturer("Ford");
 
-     car2.setEngine(engine2);
+         car2.setEngine(engine2);
 
 
+         Truck truck = new Truck("Volvo");
+         truck.setColor("white");
+         truck.setDoorCount(2);
+         truck.setMileage(9);
+         truck.setFuelLevel(65);
 
-     Truck truck = new Truck("Volvo");
-     truck.setColor("white");
-     truck.setDoorCount(2);
-     truck.setMileage(9);
-     truck.setFuelLevel(65);
+         Engine engineTruck = new Engine();
+         engineTruck.setManufacturer("Volvo");
 
-     Engine engineTruck = new Engine();
-     engineTruck.setManufacturer("Volvo");
+         truck.accelerate(7.2, 6.9);
 
-     truck.accelerate(7.2,6.9);
+         competitors[0] = car1;
+         competitors[1] = car2;
 
-     firstCompetitor = car1;
-     secondCompetitor = car2;
+         competitorsList.add(car1);
+         competitorsList.add(car2);
 
-     car1.accelerate(7.2,  6.9);
+         for (int i = 0; i < competitors.length; i++) {
+             System.out.println("Iteration number" + i);
+             System.out.println(competitors[i]);
+         }
+
+         firstCompetitor = car1;
+         secondCompetitor = car2;
+
+         car1.accelerate(7.2, 6.9);
+     }
  }
 
-     }
+
+
 
 
 
